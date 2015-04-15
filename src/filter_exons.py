34a17761a1c6f995dae930b1f4f0576415f0a845
blt2114 <blt2114@columbia.edu>
2015-04-15 16:59:45 -0400

    updated filter_exons only include exons of genes experessed in tha sample

--- src/filter_exons.py
+++ src/filter_exons.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 """
+Filter_exons goes through a file gene expression data (from raw gene RPKM
+file) and file of exon RPKM data and sel
 """
 
 import json
 import sys
+import re
+import numpy as np
+import matplotlib.pyplot as plt
 
-
-if len(sys.argv) is not 3:
+if not len(sys.argv) in [3, 4]:
     sys.stderr.write("invalid usage: python filter_exons.py"+
             " <genes_to_filter_RPKM.tsv>"+
-            " <exons_RPKM.txv>\n")
+            " <exons_RPKM.tsv> (<gene_RPKM_threshold>)\n")
     sys.exit(2)
 
 genes_fn = sys.argv[1]
 exons_fn = sys.argv[2]
+gene_RPKM_threshold = 100.0 #default threshold for gene expression in sample
+                            #for exons in that gene ot be included.
+if len(sys.argv) is 4:
+    gene_RPKM_threshold=float(sys.argv[3])
 
 # load genes info from json into a dictionary
 with open(genes_fn) as genes_file:
@@ -26,7 +34,7 @@ for gene in genes[1:]:
     gene_split = gene.strip("\n").split("\t")
     gene_dict={}
     for i in range(1,len(gene_labels)):
-        if float(gene_split[i]) == 0.0:
+        if float(gene_split[i]) <gene_RPKM_threshold:
             gene_dict[gene_labels[i]]=None
         else:
             gene_dict[gene_labels[i]]=gene_split[i]
@@ -37,22 +45,66 @@ with open(exons_fn) as exons_file:
     exons = list(exons_file)
 exons_file.close()
 
+# this function parses exon location entry in to 3 prime and 5 prime
+# locations
+def parse_3p_5p(exon_location):
+    location_dir = re.split("<",exon_location)
+    chr_loc1_loc2= re.split(":|-|<",exon_location)
+    chrom = chr_loc1_loc2[0]
+    if location_dir[1] == "-1":
+        three_p = chr_loc1_loc2[1]
+        five_p = chr_loc1_loc2[2]
+    else:
+        three_p = chr_loc1_loc2[2]
+        five_p = chr_loc1_loc2[1]
+    return chrom, five_p, three_p,int(location_dir[1])
+
+
 exons_dict={}
 exon_labels=exons[0].strip("\n").split("\t") # first row is column headings
+
+last_end =0
 for exon in exons[1:]:
     exon_split=exon.strip("\n").split("\t")
+    inclusion_vals=[]
     if not genes_dict.has_key(exon_split[1]): 
         continue
     exon_dict={}
+    
     exon_dict[exon_labels[0]]=exon_split[0]
+    exon_dict[exon_labels[1]]=exon_split[1]
+    chrom, five_p_loc,three_p_loc, read_dir = parse_3p_5p(exon_split[0])
+    exon_dict["chrom"]=chrom
+    exon_dict["read_dir"]=read_dir
+    exon_dict["five_p_loc"]=five_p_loc
+    exon_dict["three_p_loc"] = three_p_loc
+    last_end=three_p_loc
     for i in exon_labels[2:]:
         if genes_dict[exon_split[1]][i] is None:
-            exon_dict[i]=None
             continue
         gene_RPKM = float(genes_dict[exon_split[1]][i])
         exon_RPKM = float(exon_split[exon_labels.index(i)])
-        exon_dict[i]=exon_RPKM/gene_RPKM
-    exons_dict[exon_split[0]]=exon_dict
+        
+        #create dictionary that will be a data point
+        exon_dict[i]={"p_inc":exon_RPKM/gene_RPKM}
+        inclusion_vals.append(exon_RPKM/gene_RPKM)
+        
+    exons_dict[exon_split[0]]=inclusion_vals
+
     print json.dumps(exon_dict)
-#print "genes" + str(genes[0])
-#print "exons" + str(exons[0])
+
+
+if False: #set to True to produce figures
+    keys= exons_dict.keys()
+    vals = exons_dict[keys[0]]
+    count = 0
+    side_len =6
+    for i in range(0,side_len):
+        for j  in range (0,side_len):
+            count+=1
+            plt.subplot(side_len,side_len,count)
+            plt.title("mean: " +
+                str(np.mean(exons_dict[keys[count]]))[0:5])
+            plt.hist(exons_dict[keys[count]],bins=100,range=(0,3))
+            plt.axis([0,3,0,20])
+    plt.show()
