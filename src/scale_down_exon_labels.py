c6d4ef16acf6f2db6c8d3a1e8ce5b4b4bcd35f1a
blt2114 <blt2114@columbia.edu>
2015-04-18 22:43:04 -0400

    added file to scale down exon labels

--- src/scale_down_exon_labels.py
+++ src/scale_down_exon_labels.py
@@ -6,8 +6,8 @@ import sys
 import json
 
 if len(sys.argv) < 2:
-    sys.stderr.write("invalid usage: python scale_down_labels.py"+
-            " <read_counts.json>  <file_to_scale>\n")
+    sys.stderr.write("invalid usage: python scale_down_exon_labels.py"+
+            " <read_counts.json> <file_to_scale>\n")
     sys.exit(2)
 
 read_counts_fn = sys.argv[1]
@@ -24,9 +24,14 @@ dicts = []
 with open(file_to_scale_down) as json_file:
     for line in json_file:
         site=json.loads(line)
-        for k1 in site.keys():
+        for k1 in site.keys(): 
             if type(site[k1]) is dict:
+                if not reads.has_key(k1):
+                    # this is the case for exprmt E000
+                    continue
                 for k2 in site[k1].keys():
-                    site[k1][k2]["num_reads"]/= float(reads[k1][k2])
+                    mark=k2.split("_")[0]
+                    if reads[k1].has_key(mark):
+                        site[k1][k2]/= float(reads[k1][mark])
         print json.dumps(site)
 json_file.close()
