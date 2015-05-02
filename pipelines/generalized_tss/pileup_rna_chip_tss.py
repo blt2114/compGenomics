7755038e6965243207acce43d8b723c1a3514d46
zxqwerxz <zhou.jeffrey.y@gmail.com>
2015-05-02 18:44:11 -0400

    Finalized pileup code

--- pipelines/generalized_tss/pileup_rna_chip_tss.py
+++ pipelines/generalized_tss/pileup_rna_chip_tss.py
@@ -20,60 +20,62 @@ def main(argv):
 
     rna_fn = sys.argv[1]
     chip_fn = sys.argv[2]
-    progress = FileProgress(chip, "Percent Complete: ")
+    progress = FileProgress(chip_fn, "Percent Complete: ")
 
     # Sort both files lexically by chromosome, then by position
     sys.stderr.write('Beginning to sort both files (may take a while).\n')
-    rna = unix_sort(rna_fn, "-k2,2 -k4,4", header=False, save=True)
-    chip = unix_sort(chip_fn, "-t $'\t' -k1,2", header=False, save=True)
+    rna_f = unix_sort(rna_fn, "-k2,2 -k4,4", header=False, save=True)
+    chip_f = unix_sort(chip_fn, "-t $'\t' -k1,2", header=False, save=True)
     sys.stderr.write('Finished sorting.\n')
 
     # Read RNA-seq data into memory
     sys.stderr.write('Reading the RNA-seq data into memory.\n')
     dict = {}
-    with open(rna) as json_file:
-        for line in json_file:
-            site = json.loads(line)
-            seqname = site['seqname']
-            tss = site['tss']
+    for line in rna_f:
+        site = json.loads(line)
+        seqname = site['seqname']
+        tss = site['tss']
 
-            if seqname in dict:
-                if tss in dict[seqname]:
-                    print "Error!"
-                else:
-                    dict[seqname][str(tss)] = site
+        if seqname in dict:
+            if tss in dict[seqname]:
+                print "Error!"
             else:
-                dict[seqname] = { str(tss) : site }
+                dict[seqname][str(tss)] = site
+        else:
+            dict[seqname] = { str(tss) : site }
+    rna_f.close()
 
     # Begin looping through chip file
+    sys.stderr.write('Beginning to read the ChIP data.\n')
     previous_seqname = None
     previous_tss = None
-    with open(chip) as chip_file:
-        for line in chip_file:
-            chip_row = line.split("\t")
-            seqname, tss, sample, mark, rpm = (chip_row[0], chip_row[1], chip_row[2], chip_row[3], eval(chip_row[4]))
+    for line in chip_f:
+        chip_row = line.split("\t")
+        seqname, tss, sample, mark, rpm = (chip_row[0], chip_row[1], chip_row[2], chip_row[3], eval(chip_row[4]))
 
-            if previous_tss is None:
-                previous_seqname = seqname
-                previous_tss = tss
-                progress.update()
-                continue
-
-            if previous_seqname != seqname or previous_tss != tss:
-                print json.dumps(dict[previous_seqname][previous_tss])
-                dict[previous_seqname].pop(previous_tss, None)
-
-            tss_site = dict[seqname][tss]
-
-            if sample in tss_site['samples']:
-                tss_site['samples'][sample][mark] = rpm
-            else:
-                tss_site['samples'][sample] = { mark : rpm }
-
+        if previous_tss is None:
             previous_seqname = seqname
             previous_tss = tss
             progress.update()
+            continue
 
+        if previous_seqname != seqname or previous_tss != tss:
+            print json.dumps(dict[previous_seqname][previous_tss])
+            dict[previous_seqname].pop(previous_tss, None)
+
+        tss_site = dict[seqname][tss]
+
+        if sample in tss_site['samples']:
+            tss_site['samples'][sample][mark] = rpm
+        else:
+            tss_site['samples'][sample] = { mark : rpm }
+
+        previous_seqname = seqname
+        previous_tss = tss
+        progress.update()
+    print json.dumps(dict[previous_seqname][previous_tss])
+
+    chip_f.close()
     sys.stderr.write("\nAll done!\n")
 
 # Execute this module as a command line script
