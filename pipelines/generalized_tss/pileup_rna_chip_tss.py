b467fcf679e1cf4008f9de21186a02659499b71c
zxqwerxz <zhou.jeffrey.y@gmail.com>
2015-05-02 18:40:19 -0400

    I think it's finally fixed

--- pipelines/generalized_tss/pileup_rna_chip_tss.py
+++ pipelines/generalized_tss/pileup_rna_chip_tss.py
@@ -20,81 +20,59 @@ def main(argv):
 
     rna_fn = sys.argv[1]
     chip_fn = sys.argv[2]
-
-    progress = FileProgress(chip_fn, "Percent Complete: ")
+    progress = FileProgress(chip, "Percent Complete: ")
 
     # Sort both files lexically by chromosome, then by position
     sys.stderr.write('Beginning to sort both files (may take a while).\n')
-    rna_f = unix_sort(rna_fn, "-k2,2 -k4,4", header=False, save=True)
-    chip_f = unix_sort(chip_fn, "-t $'\t' -k1,2", header=False, save=True)
+    rna = unix_sort(rna_fn, "-k2,2 -k4,4", header=False, save=True)
+    chip = unix_sort(chip_fn, "-t $'\t' -k1,2", header=False, save=True)
     sys.stderr.write('Finished sorting.\n')
 
-    # Initialize relevant variables necessary for the loop (RNA FILE, outer loop)
-    rna_line = rna_f.readline()
-    tss_site = json.loads(rna_line)
-    chip_line = chip_f.readline()
-    chip_row = chip_line.split("\t")
-    seqname, pos, sample, mark, rpm = (chip_row[0], chip_row[1], chip_row[2], chip_row[3], eval(chip_row[4]))
+    # Read RNA-seq data into memory
+    sys.stderr.write('Reading the RNA-seq data into memory.\n')
+    dict = {}
+    with open(rna) as json_file:
+        for line in json_file:
+            site = json.loads(line)
+            seqname = site['seqname']
+            tss = site['tss']
 
-    # Begin reading through both files in alternation
-    while True:
+            if seqname in dict:
+                if tss in dict[seqname]:
+                    print "Error!"
+                else:
+                    dict[seqname][str(tss)] = site
+            else:
+                dict[seqname] = { str(tss) : site }
 
-        # If this position has not reached the tss site, increment the position
-        if seqname < tss_site['seqname'] or int(pos) < tss_site['tss']:
-            chip_line = chip_f.readline()
-            if not chip_line:
-                print json.dumps(tss_site)
-                count = 0
-                while True:
-                    count += 1
-                    if not chip_f.readline(): break
-                print "chip left: " + count
-                break
-            chip_row = chip_line.split("\t")
-            seqname, pos, sample, mark, rpm = (chip_row[0], chip_row[1], chip_row[2], chip_row[3], eval(chip_row[4]))
+    # Begin looping through chip file
+    previous_seqname = None
+    previous_tss = None
+    with open(chip) as chip_file:
+        for line in chip_file:
+            chip_row = line.split("\t")
+            seqname, tss, sample, mark, rpm = (chip_row[0], chip_row[1], chip_row[2], chip_row[3], eval(chip_row[4]))
+
+            if previous_tss is None:
+                previous_seqname = seqname
+                previous_tss = tss
+                progress.update()
+                continue
+
+            if previous_seqname != seqname or previous_tss != tss:
+                print json.dumps(dict[previous_seqname][previous_tss])
+                dict[previous_seqname].pop(previous_tss, None)
+
+            tss_site = dict[seqname][tss]
+
+            if sample in tss_site['samples']:
+                tss_site['samples'][sample][mark] = rpm
+            else:
+                tss_site['samples'][sample] = { mark : rpm }
+
+            previous_seqname = seqname
+            previous_tss = tss
             progress.update()
-            continue
-
-        # If this position has passed the tss site, increment the tss site
-        if seqname > tss_site['seqname'] or int(pos) > tss_site['tss']:
-
-            print json.dumps(tss_site)
-
-            rna_line = rna_f.readline()
-            if not rna_line:
-                count = 0
-                while True:
-                    count += 1
-                    if not chip_f.readline(): break
-                print "chip left: " + count
-                break
-            tss_site = json.loads(rna_line)
-
-            continue
-
-        # At this point, both the tss site and the chip position are equal
-        # We can do the main evaluating and processing
-        if sample in tss_site['samples']:
-            tss_site['samples'][sample][mark] = rpm
-        else:
-            tss_site['samples'][sample] = { mark : rpm }
-
-        # Increment to the next chip position in the loop
-        chip_line = chip_f.readline()
-        if not chip_line:
-            print json.dumps(tss_site)
-            count = 0
-            while True:
-                count += 1
-                if not rna_f.readline(): break
-            print "rna left: " + count
-            break
-        chip_row = chip_line.split("\t")
-        seqname, pos, sample, mark, rpm = (chip_row[0], chip_row[1], chip_row[2], chip_row[3], eval(chip_row[4]))
-        progress.update()
-
-    rna_f.close()
-    chip_f.close()
 
     sys.stderr.write("\nAll done!\n")
 
