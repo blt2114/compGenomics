4be90564499634a50c5803114bffb248ae2d2cd6
zxqwerxz <zhou.jeffrey.y@gmail.com>
2015-05-09 03:36:09 -0400

    Cpp script is working

--- src/ChIP/run_download_and_extract.sh
+++ src/ChIP/run_download_and_extract.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
-chr=""
-if [ "$#" -ne 6 ];then
-    echo "./run_download_and_extract.sh <working_dir> <output_dir> <ChIP_experiment_list> <sites.json> <num_batches> <config file> (<chr#>)"
-    exit
+
+if [ "$#" -eq 6 ];then
+
+    wd=$1
+    out_dir=$2
+    ChIP_expr_list=$3
+    sites_fn=$4
+    num_batches=$5
+    config=$6
+
+    total_lines=`wc -l<$ChIP_expr_list`
+    part_len=`expr $total_lines / $num_batches`
+    part_len=`expr $part_len + 1` #round up so no experiments are missed
+    ChIP_part_prefix=$wd"/"ChIP_File_
+    split -l $part_len $ChIP_expr_list $ChIP_part_prefix
+    ChIP_parts=`ls $ChIP_part_prefix*`
+
+    for part in $ChIP_parts; do
+
+        echo "command: ./src/ChIP/download_and_extract_read_counts.sh $wd $out_dir $part $sites_fn $config "
+        ./src/ChIP/download_and_extract_read_counts.sh $wd $out_dir $part $sites_fn $config> $part"_log" 2>&1 &
+
+    done
+
+
+else
+    if [ "$#" -eq 7 ]; then
+
+        wd=$1
+        out_dir=$2
+        ChIP_expr_list=$3
+        sites_fn=$4
+        num_batches=$6
+        config=$5
+
+        total_lines=`wc -l<$ChIP_expr_list`
+        part_len=`expr $total_lines / $num_batches`
+        part_len=`expr $part_len + 1` #round up so no experiments are missed
+        ChIP_part_prefix=$wd"/"ChIP_File_
+        split -l $part_len $ChIP_expr_list $ChIP_part_prefix
+        ChIP_parts=`ls $ChIP_part_prefix*`
+
+        for part in $ChIP_parts; do
+
+            echo "command: ./src/ChIP/download_and_extract_read_counts.sh $wd $out_dir $part $sites_fn $config 1"
+            ./src/ChIP/download_and_extract_read_counts.sh $wd $out_dir $part $sites_fn $config 1 > $part"_log" 2>&1 &
+
+        done
+
+    else
+        echo "./run_download_and_extract.sh <working_dir> <output_dir> <ChIP_experiment_list> <sites.json> <config file> <num_batches> (true-if-files-exist-already)"
+        exit
+    fi
 fi
 
-wd=$1
-out_dir=$2
-ChIP_expr_list=$3
-sites_fn=$4
-num_batches=$5
-config=$6
-
-total_lines=`wc -l<$ChIP_expr_list`
-
-part_len=`expr $total_lines / $num_batches`
-part_len=`expr $part_len + 1` #round up so no experiments are missed 
-
-ChIP_part_prefix=$wd"/"ChIP_File_
-
-split -l $part_len $ChIP_expr_list $ChIP_part_prefix
-
-ChIP_parts=`ls $ChIP_part_prefix*` 
-
-for part in $ChIP_parts; do
-
-    echo "command: ./src/ChIP/download_and_extract_read_counts.sh $wd $out_dir $part $sites_fn $config "
-    ./src/ChIP/download_and_extract_read_counts.sh $wd $out_dir $part $sites_fn $config> $part"_log" 2>&1 &
-
-done
--- src/old_unused_code/xtract_chip_all_tss.py
+++ src/old_unused_code/xtract_chip_all_tss.py
@@ -9,135 +9,9 @@ __author__ = 'jeffrey'
 import sys, json, os, collections, itertools, traceback
 import Queue, threading, subprocess
 
-# Global Settings
-RECENT_GENE_BUFFER_LENGTH = 15000
-CHIP_BUFFER_LENGTH = 2000
-
 # Global variables
 count_files = 0
 total_files = 0
-
-class PrintThread(threading.Thread):
-    def __init__(self, queue, outfile):
-        threading.Thread.__init__(self)
-        self.queue = queue
-        self.outfile = outfile
-        self.keepRunning = True
-
-    def run(self):
-        f = open(self.outfile, "w")
-        while self.keepRunning:
-            result = self.queue.get()
-            f.write(result + "\n")
-            f.flush()
-            self.queue.task_done()
-        f.close()
-
-def child(chip_fn, rna_fn, reads_fn, window_size, bin_size, out_queue):
-
-    # load configuration file(s)
-    with open(reads_fn) as experiment_read_counts_file:
-        read_counts = json.load(experiment_read_counts_file)
-
-    # Parse sample information from file name
-    # The correction will be reads starts per million (RPM)
-    sample = os.path.basename(chip_fn).split('-')[0]
-    mark = chip_fn.split('-')[1].replace('.tagAlign','')
-    correction = float(read_counts[sample][mark]) / 1000000
-
-    # load sites of interest from json into a list
-    tss_file = open(rna_fn)
-    tss_site = json.loads(tss_file.readline())
-
-    # Initialize this tss site
-    window_start = tss_site['tss'] - window_size
-    window_end = tss_site['tss'] + window_size
-    num_bins = (window_end - window_start) / (bin_size)
-    if sample in tss_site['samples']:
-        if mark in tss_site['samples'][sample]:
-            tss_site['samples'][sample][mark] = [0] * num_bins
-        else:
-            tss_site['samples'][sample] = { mark : [0] * num_bins }
-    else:
-        tss_site['samples'] = { sample : { mark : [0] * num_bins } }
-
-    # Move through both the TSS and CHIP Files
-    recent_genes = collections.deque()
-    f = open(chip_fn, 'r')
-    EOF = False
-    while not EOF:
-
-        # When there are no more TSS sites, break
-        if tss_site == None: break
-
-        # Grab a chunk of lines from the CHIP file, of specified buffer size
-        lines = collections.deque(itertools.islice(f, CHIP_BUFFER_LENGTH))
-        if not lines:
-            EOF = True
-
-        # Process the buffer
-        while len(lines) != 0:
-
-            # When there are no more TSS sites, break
-            if tss_site == None: break
-
-            line = lines.popleft()
-            recent_genes.append(line)
-            if len(recent_genes) > RECENT_GENE_BUFFER_LENGTH:
-                recent_genes.pop()
-
-            # parse out the read location
-            read_seqname = line.split("\t")[0]
-            read_pos = int(line.split("\t")[1])
-
-            # If the read is before the lower boundary of the window, skip this loop
-            if read_seqname != tss_site['seqname'] or read_pos < window_start:
-                continue
-
-            # If the read is within the boundaries, add one to the count and continue
-            if read_pos <= window_end:
-                bag = (read_pos - window_start) / bin_size
-                if bag == num_bins:
-                    bag -= 1
-                tss_site['samples'][sample][mark][bag] += 1
-                continue
-
-            # By this point, read is upstream of the upper boundary of the window,
-
-            # While no label available, so skip to next site.
-            # Reverse the bins if the sequence on reverse strand to capture upstream/downstream
-            if tss_site['strand'] == '-':
-                tss_site['samples'][sample][mark].reverse()
-            tss_site['samples'][sample][mark][:] = [x / correction for x in tss_site['samples'][sample][mark]]
-
-            # Print!
-            result = tss_site['seqname'] + "\t" + str(tss_site['tss']) + "\t" + sample + "\t" + mark + "\t" + json.dumps(tss_site['samples'][sample][mark])
-            #print tss_site['seqname'] + "\t" + str(tss_site['tss']) + "\t" + sample + "\t" + mark + "\t" + json.dumps(tss_site['samples'][sample][mark])
-            out_queue.put(result)
-
-            # Read the next TSS site and initialize
-            current_tss_line = tss_file.readline()
-            if current_tss_line == "":
-                tss_site = None
-                continue
-            tss_site = json.loads(current_tss_line)
-            window_start = tss_site['tss'] - window_size
-            window_end = tss_site['tss'] + window_size
-            num_bins = (window_end - window_start) / (bin_size)
-            if sample in tss_site['samples']:
-                if mark in tss_site['samples'][sample]:
-                    tss_site['samples'][sample][mark] = [0] * num_bins
-                else:
-                    tss_site['samples'][sample] = { mark : [0] * num_bins }
-            else:
-                tss_site['samples'] = { sample : { mark : [0] * num_bins } }
-
-            # Add the recent genes onto the beginning of the lines to process
-            # This allows lines to be counted towards multiple sites
-            lines.extendleft(recent_genes)
-            recent_genes.clear()
-    f.close()
-    tss_file.close()
 
 class ProcessThread(threading.Thread):
     def __init__(self, in_queue, out_queue):
@@ -169,24 +43,23 @@ class ProcessThread(threading.Thread):
 def main(argv):
 
     # parse args
-    if len(sys.argv) is not 8:
+    if len(sys.argv) is not 8 or len(sys.argv) is not 7:
         sys.stderr.write("invalid usage: python " + sys.argv[0] +
-                    " <all_tss_rna.json> <chip_directory> <outfile> <experiment_read_counts.json> <window_size> <bin_size> <threads>\n")
+                    " <sites.json> <working_dir> <out_dir> <experiment_list> <config-file> <threads> <download? T/F>\n")
         sys.exit(2)
 
     rna_fn = sys.argv[1]
     chip_dir = sys.argv[2]
     out_fn = sys.argv[3]
-    reads_fn = sys.argv[4]
-    window_size = int(sys.argv[5])
-    bin_size = int(sys.argv[6])
-    num_threads = int(sys.argv[7])
+    files = sys.argv[4]
+    config = sys.argv[5]
+    num_threads = int(sys.argv[6)
 
     # Locate filelist
     filelist = []
-    for file in os.listdir(chip_dir):
-        if file.endswith(".tagAlign"):
-            filelist.append(os.path.join(chip_dir, file))
+    with open(files) as f:
+        for line in f:
+            filelist.append()
     sys.stderr.write("A total of " + str(len(filelist)) + " files have been located in " + chip_dir + ".\n")
     global total_files
     total_files = len(filelist)
@@ -202,11 +75,6 @@ def main(argv):
         t.setDaemon(True)
         t.start()
         threads.append(t)
-
-    # spawn threads to print
-    t = PrintThread(resultqueue, out_fn)
-    t.setDaemon(True)
-    t.start()
 
     # add paths to queue
     for path in paths:
